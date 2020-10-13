78518ca911214962d57cf6328d2a2115022dfd3d
Todd Schanzlin <toddschanzlin@yahoo.com>
2020-10-12 22:11:29 -0400

    Step-1 Fully Working

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -41,15 +41,15 @@ function createMap(quakeInfo) {
     };
 
     // Create the map object with options
-    var map = L.map("mapid", {
-        center: [40.73, -74.0059],
-        zoom: 5,
+    var map = L.map("map", {
+        center: [39.828, -1.000],
+        zoom: 2,
         layers: [lightmap, quakeInfo]
     });
 
     // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
     L.control.layers(baseMaps, overlayMaps, {
-        collapsed: true
+        collapsed: false
     }).addTo(map);
 }
 
@@ -62,26 +62,38 @@ function createMarkers(response) {
 
     // Initialize array to hold earthquake markets
     let quakeMarkers = [];
+    let quakeCircles = [];
 
     // Loop through the earthquakes array to pull lat / long info:
     for (var index = 0; index < earthquakes.length; index++) {
         let coords = earthquakes[index].geometry.coordinates
 
         // For each earthquake, create marker and bind a popup with the quake's location
-        let quakeMarker = L.marker([coords[0], coords[1]])
-            .bindPopup("<h4>Magnitude:" + earthquakes[index].properties.mag + "</h4>");
+        let quakeMarker = L.marker([coords[1], coords[0]])
+            .bindPopup("<h3>Location:" + earthquakes[index].properties.place + "</h3>" + "<h4>Magnitude:" + earthquakes[index].properties.mag + "</h4>");
+
+        // Alternative quakecircle market
+        let quakeCircle = L.circle([coords[1], coords[0]], {
+            color: 'blue',
+            fillColor: 'blue',
+            fillOpacity: coords[2] / 100,
+            radius: (earthquakes[index].properties.mag * 50000)
+        }).bindPopup(`Location: ${earthquakes[index].properties.place} 
+                                     <br> Magnitude: ${earthquakes[index].properties.mag}
+                                     <br> Depth:  ${coords[2]} KM`);
+
 
         // Add marker to quakeMarkers array
         quakeMarkers.push(quakeMarker);
+        quakeCircles.push(quakeCircle);
     }
-    // Create the layer group made fro quakeMarkers array
-    createMap(L.layerGroup(quakeMarkers));
+    // Create the layer group made fro quakeMarkers array (chose "quakeCircles" or "quakeMarkers" depending on choice of marker format)
+    createMap(L.layerGroup(quakeCircles));
 
-    console.log(quakeMarkers)
+    // console.log(quakeMarkers.length)
 }
 
 // Perform API call to the earthquakes JSON file
 
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson", createMarkers);
-
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", createMarkers);
 
--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # Leaflet-Challenge
 
+
+To Do's:
+- add tectonic plate info
+
+10/13:
+- Changed markers to circles; size and opacity correspond to magnitude and depth
+- Added data labels
+
+
 10/12:
-- Pulled JSON object and reviewed format; pulling coordinate fino
+- Pulled JSON object and reviewed format; pulling coordinate info; working with simple monthly file with only 6 datapoints
 - Created base map
 - Created overlay map
+- Changed html id of map from "mapid" to "map" in order for map object to load; division reference was correct but map wouldn't load with id of "mapid"
+- Correcting location data; coordinates stored as long / lat, not lat / long
+- Confirmed geolocation data with google maps; location markers plotting correctly
+
