82973ff415c3fce40e00e66c31808c243dfe1f10
Todd Schanzlin <toddschanzlin@yahoo.com>
2020-10-12 20:54:23 -0400

    Second uplaod, JS program working without error, but map not loading

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -2,16 +2,56 @@
 
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson", function (data) {
 
+    console.log("Sample JSON response object")
     console.log(data.features);
+    console.log("-----------------------------------------------------")
 
     let features = data.features;
+
+    console.log("Confirm JSON location of coordinate and depth info ")
 
     for (var index = 0; index < features.length; index++) {
         let coords = features[index].geometry.coordinates
         console.log(coords)
     };
+    console.log("-----------------------------------------------------")
 
 });
+
+// Function to create the map
+
+function createMap(quakeInfo) {
+
+    // Create the tile layer that will be the background of our map
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "light-v10",
+        accessToken: API_KEY
+    });
+
+    // Create a baseMaps object to hold the lightmap layer
+    var baseMaps = {
+        "Light Map": lightmap
+    };
+
+    // Create an overlayMaps object to hold the quakeInfo layer
+    var overlayMaps = {
+        "Earthquakes": quakeInfo
+    };
+
+    // Create the map object with options
+    var map = L.map("mapid", {
+        center: [40.73, -74.0059],
+        zoom: 5,
+        layers: [lightmap, quakeInfo]
+    });
+
+    // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: true
+    }).addTo(map);
+}
 
 // Function to create a layer group from earhtquake markers arra; pull earthquake coordinates and info from JSON repsonse object, append to markers array 
 
@@ -24,28 +64,24 @@ function createMarkers(response) {
     let quakeMarkers = [];
 
     // Loop through the earthquakes array to pull lat / long info:
-    for (var index = 0; index < eatherquakes.length; index++) {
+    for (var index = 0; index < earthquakes.length; index++) {
         let coords = earthquakes[index].geometry.coordinates
 
         // For each earthquake, create marker and bind a popup with the quake's location
         let quakeMarker = L.marker([coords[0], coords[1]])
-            .bindPopup("<h4>" + earthquakes[index].properties.mag);
+            .bindPopup("<h4>Magnitude:" + earthquakes[index].properties.mag + "</h4>");
 
         // Add marker to quakeMarkers array
         quakeMarkers.push(quakeMarker);
-
-        createMarkers.Map(L.layerGroup(quakeMarkers));
     }
+    // Create the layer group made fro quakeMarkers array
+    createMap(L.layerGroup(quakeMarkers));
+
+    console.log(quakeMarkers)
 }
 
+// Perform API call to the earthquakes JSON file
 
-// Function to create the map
-
-function createMap() {
-
-
-}
-
-
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson", createMarkers);
 
 
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Leaflet-Challenge
+
+10/12:
+- Pulled JSON object and reviewed format; pulling coordinate fino
+- Created base map
+- Created overlay map
